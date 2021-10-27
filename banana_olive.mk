343a41d98f5d959aff0dc0144d515bcf11fd65dd
skittels15 <50242333+skittels15@users.noreply.github.com>
2021-10-27 16:38:47 +0300

    Fixed dependencies

--- banana_olive.mk
+++ banana_olive.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/banana/config/common_full_phone.mk)
+$(call inherit-product, vendor/banana/config/common.mk)
 
 # Inherit from olive device
 $(call inherit-product, device/xiaomi/olive/device.mk)
